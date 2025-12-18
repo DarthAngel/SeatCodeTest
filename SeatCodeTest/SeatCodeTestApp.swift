8f88fe39c47707851b8bc1cebf963f9ea23572a9
Angel Docampo <mail@angeldocampo.com>
2025-12-18 02:40:47 +0100

    Initial commit

---
+++ SeatCodeTest/SeatCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatCodeTestApp.swift
+//  SeatCodeTest
+//
+//  Created by Angel Docampo on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SeatCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
